2ab9446be956fe520b2ab6d927cff66c3c9ae398
tysonOliveira <thaisonoliveira@gmail.com>
2022-05-25 10:54:01 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import { RepositoryList } from "./components/RepositoryList";
+
+import './styles/global.scss';
+
+export function App() {
+  //throw new Error("erro");
+  return <RepositoryList />
+}
